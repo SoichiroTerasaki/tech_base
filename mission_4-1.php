4d7ad7e0bf41de631753534bbd977f66c70ff929
SoichiroTerasaki <67906642+SoichiroTerasaki@users.noreply.github.com>
2020-07-07 19:21:21 +0900

    Update mission_4-1.php

--- mission_4-1.php
+++ mission_4-1.php
@@ -5,9 +5,14 @@
 	// ・パスワード：**********
 
 	// DB接続設定
-	$dsn = 'mysql:dbname=tb******db;host=localhost';
-	$user = 'tb-******';
-	$password = '**********';
+	$dsn = 'mysql:dbname=tb******db;host=localhost'; #左から順にデータベースの種類（MySQL）、名前(tb******db)、ホスト(localhost)を表す。今回はホストは気にしなくていいです。
+	$user = 'tb-******'; #ポータルのログインIDと同じです。
+	$password = '**********'; #ポータルのログインパスワードと同じです。
+
+	#PDO(PHP Data Object)は、PHPからデータベースに接続して様々な処理をするための機能。
+	#new PDOで、php手元のphpからデータベースに接続する。
+	#$dsnはデータベースの情報、$userと$passwordは認証に使われる。
 	$pdo = new PDO($dsn, $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
-    echo "ok";
+
+    echo "ok"; #これでデータベースへの接続が完了。
 ?>
